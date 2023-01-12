dcecc131f5404b2f1815ce3bea9145c6443003fa
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-01-12 03:31:21 +0100

    function createRandomImage

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+## Import
+
+import random
+
 ## Constant
 
-cst_n = 256
+cst_n = 3
 cst_grayLength = 256
 cst_alpha = 0.1
 cst_h = 4
@@ -9,15 +13,21 @@ cst_w = 10
 ## Function
 
 def createRandomImage():
-    return
+    randomImage = []
+    for i in range(cst_n):
+        line = []
+        for j in range(cst_n):
+            line.append(random.randint(0, cst_grayLength-1))
+        randomImage.append(line)
+    return randomImage
 
-def transformMatrixToVector():
+def transformMatrixToVector(matrix):
     return
 
 def leastSignificantBitOfNumber(number):
     return
 
-def leastSignificantBitForVector():
+def leastSignificantBitForVector(vector):
     return
 
 def createX(image):
