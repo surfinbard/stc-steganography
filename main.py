628b3cdaf2dc58fe418616e39d36ea82e29ee0d3
Beatriz Rochelle <beatriz@hoxwi.com>
2023-01-13 06:04:20 +0100

    feat: added new functions for trellis
    updated old functions

--- main.py
+++ main.py
@@ -1,160 +1,159 @@
 import random
+import array as arr
+import numpy as np
 
-## Constants
+# size of edges of cover image
+edgeSize = 3  
+# 8bit grayscale pixel range
+pixelRange = 256
+# alpha = messageLength / edgeSize
+alpha = 1  # 0.1
+# size of the subMatrix
+subHeight = 2
+subWidth = 2
 
-# cst_n x cst_n is the size of the image
-cst_n = 3  # 256
+# Returns empty matrix H
+def generateH():
+    H = []
+    for i in range(edgeSize * alpha):
+        H.append([])
+        for j in range(edgeSize ** 2):
+            H[i][j] = None
+    return H
 
-# 8-bits of gray
-cst_grayLength = 256
-
-# cst_alpha = messageLength / cst_n
-cst_alpha = 1  # 0.1
-
-# size of the submatrix
-cst_h = 4
-cst_w = 10
-
-## Functions
-
-# "matrix" refers to a list of list
-# "vector" refers to a list
-
-# Returns matrix of size cst_n x cst_n with values being a random value between 0 and cst_grayLength - 1
-def createRandomImage():
-    randomImage = []
-    for i in range(cst_n):
-        line = []
-        for j in range(cst_n):
-            line.append(random.randint(0, cst_grayLength - 1))
-        randomImage.append(line)
-    return randomImage
-
-
-# Returns a vector of length cst_n * cst_n that contains all the values of given matrix of size cst_n x cst_n
-def matrixToVector(matrix):
-    vector = []
-    for i in range(cst_n):
-        for j in range(cst_n):
-            vector.append(matrix[i][j])
-    return vector
-
-
-# Returns a matrix of size cst_n x cst_n that contains all the values of vector of size cst_n * cst_n
-# The i-th cst_n values are ranged in the i-th row.
-def vectorToMatrix(vector):
-    matrix = []
-    for i in range(cst_n):
-        matrix.append([])
-        for j in range(cst_n):
-            matrix[i].append(vector[i * cst_n + j])
-    return matrix
-
-
-# Returns the value of the least significant bit of a given binary number
-def binaryNumberLSB(number):
-    return number % 2
-
-
-# Returns a vector of least significant bits of another vector
-def vectorLSB(vector):
-    result = []
-    for i in range(len(vector)):
-        result.append(binaryNumberLSB(vector[i]))
-    return result
-
-
-# Returns vector x of given image
-def createX(image):
-    return vectorLSB(matrixToVector(image))
-
-
-# Returns the submatrix used to create matrix H
-def createSubH():
+# Returns subMatrix
+def generateSubH():
     subH = [[1, 0], [1, 1]]
     return subH
 
+# Generates message
+def generateRandomMsg():
+    return random.randint(range(edgeSize * alpha))
 
-# Returns the matrix with adequately placed submatrix
-# Note : Affects matrix
-def placeSubmatrixInMatrix(submatrix, matrix, i, j):
-    h = len(submatrix)
-    w = len(submatrix[0])
-    H = len(matrix)
-    W = len(matrix[0])
-    for k in range(h):
-        for l in range(w):
-            if (i + k < H) & (j + l < W):
-                matrix[i + k][j + l] = submatrix[k][l]
+def generateRandomImg():
+    randomImage = []
+    for i in range(edgeSize):
+        for j in range(edgeSize):
+            randomImage[i].append((random.randint(0, pixelRange - 1)))
+    return randomImage
+
+def fillH(H, subH):
+    matrixHeight = len(H[0])
+    # all height indexes will be used but not all width indexes, hence j on the outside (easier)
+    for j in matrixHeight:
+        for i in range(len(H)):
+            if (i % subWidth == 0):
+                # placing subH inside H, item by item
+                for x in subWidth:
+                    for y in subHeight:
+                        H[i][j] = subH[x][y]
+
+# Outputs matrix in vector format
+def matrixToVector(matrix):
+    vector = []
+    for i in range(edgeSize):
+        for j in range(edgeSize):
+            vector.append(matrix[i][j])
+    return vector
+
+# Builds matrix out of vector
+def vectorToMatrix(vector):
+    matrix = []
+    for i in range(edgeSize):
+        matrix.append([])
+        for j in range(edgeSize):
+            matrix[i].append(vector[i * edgeSize + j])
     return matrix
 
+# Gets LSB of binary number
+def getLSB(number):
+    return number % 2
 
-# Returns the matrix H, given submatrix
-def createH(subH):
-    w = len(subH[0])
-    H = []
-    for i in range(cst_n * cst_alpha):
-        H.append([])
-        for j in range(cst_n**2):
-            H[i].append(0)
+# Converts vector of binary numbers to LSBs only
+def LSBVector(vector):
+    result = []
+    for i in range(len(vector)):
+        result.append(getLSB(vector[i]))
+    return result
 
-    for b in range(len(H)):
-        H = placeSubmatrixInMatrix(subH, H, b, b * w)
+# Converts image to LSB vector
+def imgToLSBVector(image):
+    return LSBVector(matrixToVector(image))
 
-    return H
-
-
-# Returns a vector of length cst_n * cst_alpha, which represents a message
-# Its values represent randomly generated bits
-def createRandomM():
-    m = []
-    for i in range(cst_n * cst_alpha):
-        m.append(random.randint(0, 1))
-    return m
-
-
-# todo
 # Returns optimal y after trellis construction
 def syndromeTrellis(H, subH, x):
     return
 
+def getColumnForTrellis(index, H):
+    transposedH = np.transpose(H)
+    if index % 2 == 0:
+        return transposedH[index]
+    else:
+        return arr.reversed(transposedH[index])
 
-# Returns the stego image, given image matrix, vectors x and y
-def createStegoImage(image, x, y):
+def moveBetweenBlocks(i):
+    if (i == (0 or 1)):
+        return 0
+    if (i == (10 or 11)):
+        return 1
+
+def moveInsideBlocks(trellis, H):
+    for i in range(subWidth):
+        for j in range(len(trellis[i])):
+            if (node.data):  
+                state = bin(j)    
+                node = trellis[i][j]
+                nextNode1 = trellis[i+1][j]
+                
+                node.next1 = nextNode1
+                nextNode1.prev1 = node
+
+                node.next2 = state ^ getColumnForTrellis(i, H)
+
+def getWeight(anything):
+    return
+
+def viterbi(trellis):
+    return
+
+def getStegoImage(image, x, y):
     stegoImage = []
     differenceVector = []
+
     for i in range(len(x)):
         differenceVector.append(y[i] - x[i])
     differenceMatrix = vectorToMatrix(differenceVector)
+
     for i in range(len(image)):
         stegoImage.append([])
         for j in range(len(image[0])):
             stegoImage[i].append(image[i][j] + differenceMatrix[i][j])
     return stegoImage
 
-
-# todo
-# Returns distortion between image and stego image
 def totalDistortion(image, stegoImage):
-    return
+    sum = 0
+    for i in range(len(image)):
+        for j in range(image[j]):
+            if (image[i][j] != stegoImage[i][j]):
+                sum += 1
+    return sum
 
-
-# todo
-# Returns a visual representation of the final result
+# Outputs visual representation of the final result
 def showResult(image, stegoImage):
     return
 
 
-## Initialize Data
+if __name__ == '__main__':
 
-v_image = createRandomImage()
-v_subH = createSubH()
-v_H = createH(v_subH)
-v_m = createRandomM()
+    ## Initialize data
+    randomImg = generateRandomImg()
+    subH = generateSubH()
+    H = generateH()
+    fillH(H, subH)
+    message = generateRandomMsg()
 
-## Run
-
-v_x = createX(v_image)
-v_y = syndromeTrellis(v_H, v_x)
-# v_stegoImage = createStegoImage(v_image, v_x, v_y)
-# showResult(v_image, v_stegoImage)
+    ## Run
+    cover = imgToLSBVector(randomImg)
+    optimalY = syndromeTrellis(H, cover)
+    # v_stegoImage = getStegoImage(randomImg, cover, optimalY)
+    # showResult(randomImg, v_stegoImage)
