dd0806679b839a28069ac02cab076395013a87a9
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-02-03 07:02:45 +0100

    feat: show efficiency

--- STC.py
+++ STC.py
@@ -390,8 +390,11 @@ if __name__ == '__main__':
 
             embed()
             extract(h)
-            print("With a distortion of :", calculate_distortion(Image.open(path).convert('L'), stego_img))
+
+            distortion = calculate_distortion(Image.open(path).convert('L'), stego_img)
+            print("With a distortion of :", distortion)
             print("For a message of length :", len(message))
+            print("Which give an efficiency of :", get_efficiency(len(message), distortion))
         case '2':
             messages = get_random_payloads(10, 48)
             for msg in messages:
